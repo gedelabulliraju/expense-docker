21c0f4399551e44f890464dd59be91b47d76344c
Bulliraju <bulliraju118@gmail.com>
2025-06-27 19:07:33 +0530

    docker

--- resizedisk.sh
+++
@@ -1,9 +0,0 @@
-# Description: This script resizes the disk partitions and filesystems on a Linux system.
-# Usage: Run this script with root privileges to resize the partitions and filesystems.     
-#!/bin/bash
-lsblk
-sudo growpart /dev/nvme0n1 4
-sudo lvextend -l +50%FREE /dev/RootVG/rootVol
-sudo lvextend -l +50%FREE /dev/RootVG/varVol
-sudo xfs_growfs /
-sudo xfs_growfs /var
